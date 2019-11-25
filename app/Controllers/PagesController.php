6c0ffd564985d11df639005451eb7206e18e02bf
Flachag <flavien.chagras@outlook.fr>
2019-11-25 17:07:52 +0100

    Update pageController

---
+++ app/Controllers/PagesController.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Controllers;
+
+use App\models\Item;
+use App\models\Liste;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class PagesController extends MainController {
+
+    public function home(RequestInterface $request, ResponseInterface $response){
+        $this->render($response,'pages/home.twig', ["current_page" => "home"]);
+    }
+
+    public function getListes(RequestInterface $request, ResponseInterface $response){
+        $this->render($response,'pages/listes.twig',  ["current_page" => "voir_listes", "listes" => Liste::all()]);
+    }
+
+    public function getItems(RequestInterface $request, ResponseInterface $response){
+        $get = explode("=", $request->getUri()->getQuery());
+        $liste = Liste::where('token', $get[1]);
+
+        if ($get[0] == "liste" && sizeof($get)==2 && $liste->count()==1) {
+            $liste = $liste->first();
+            $this->render($response, 'pages/items.twig', ["current_page" => "voir_objets",
+                "items" => Item::where('liste_id', $liste->no)->get(),
+                "liste" => $liste]);
+        } else {
+            $this->render($response, 'pages/404.twig', ["current_page" => "404"]);
+        }
+    }
+}
