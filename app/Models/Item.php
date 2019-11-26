7a5e639bb336fbc9dbd86ea3a20a06e4e89c39b8
Flachag <flavien.chagras@outlook.fr>
2019-11-26 12:36:05 +0100

    Optimisation de PageController, modification dans les models

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -7,8 +7,14 @@ class Item extends Model {
     protected $table = 'item';
     protected $primaryKey = 'id' ;
     public $timestamps = false;
+    protected $appends = ['reservation'];
 
     public function liste(){
-        return $this->belongsTo('Models\Liste', 'no');
+        return $this->belongsTo('App\Models\Liste', 'no');
+    }
+
+    public function getReservationAttribute()
+    {
+        return $this->attributes['reservation'] = Reservation::where('id_item',$this->id)->count();
     }
 }
--- app/Models/Liste.php
+++ app/Models/Liste.php
@@ -7,8 +7,14 @@ class Liste extends Model{
     protected $table = 'liste';
     protected $primaryKey = 'id' ;
     public $timestamps = false;
+    protected $appends = ['items'];
 
     public function item(){
-        return $this->hasMany('Models\Item', 'liste_id');
+        return $this->hasMany('App\Models\Item', 'liste_id');
+    }
+
+    public function getItemsAttribute()
+    {
+        return $this->attributes['items'] = Item::where('liste_id', $this->no)->get();
     }
 }
