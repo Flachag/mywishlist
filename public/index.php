9752efd483f512e38afb2a0b25d48c53475fcad1
Blaise Lucas <lucas.blaise1@etu.univ-lorraine.fr>
2019-11-27 23:45:00 +0100

    creation item | modification item | suppression item

--- public/index.php
+++ public/index.php
@@ -23,4 +23,12 @@ $app->get('/creer-liste',PagesController::class.':getListeCreate')->setName('lis
 $app->get('/modifier-liste',PagesController::class.':getListeEditor')->setName('listeEditor');
 $app->post('/creer-liste',PagesController::class.':postListe')->setName('listeSend');
 $app->post('/modifier-liste',PagesController::class.':postListe')->setName('listeSend');
+
+$app->get('/creer-item',PagesController::class.':getItemCreate')->setName('itemCreate');
+$app->get('/modifier-item',PagesController::class.':getItemEditor')->setName('itemEditor');
+$app->post('/creer-item',PagesController::class.':postItem')->setName('itemSend');
+$app->post('/modifier-item',PagesController::class.':postItem')->setName('itemSend');
+$app->get('/supprimer-item',PagesController::class.':deleteItem')->setName('itemDelete');
+
+
 $app->run();
