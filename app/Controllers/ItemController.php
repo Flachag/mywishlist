1e717001f5b929cb4c0d8f6ee1cc5c49d83160c8
Flachag <flavien.chagras@outlook.fr>
2019-12-09 16:55:02 +0100

    Refactor des Controllers

--- app/Controllers/ItemController.php
+++ app/Controllers/ItemController.php
@@ -1,6 +1,8 @@
 <?php
 
+
 namespace App\Controllers;
+
 
 use App\models\Item;
 use App\models\Liste;
@@ -8,27 +10,16 @@ use App\Models\Reservation;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
-class PagesController extends MainController
-{
+class ItemController extends MainController{
 
-    public function home(RequestInterface $request, ResponseInterface $response)
-    {
-        $this->render($response, 'pages/home.twig', ["current_page" => "home"]);
-    }
-
-    public function getListes(RequestInterface $request, ResponseInterface $response)
-    {
-        $this->render($response, 'pages/listes.twig', ["current_page" => "voir_listes", "listes" => Liste::all()]);
-        $items = Item::all();
-        foreach ($items as $item){
-            if (!empty($item->img) && !str_contains($item->img, "assets/img/")) {
-                $headers = @get_headers($item->img);
-                if ($headers == false) {
-                    $img = "assets/img/" . $item->img;
-                    $item->img = $img;
-                    $item->save();
-                }
-            }
+    public function getItemCreate(RequestInterface $request, ResponseInterface $response){
+        $get = explode("=", $request->getUri()->getQuery());
+        $liste = Liste::where('token', $get[1]);
+        if ($liste->count() == 1) {
+            $liste = $liste->first();
+            $this->render($response, 'pages/itemCreate.twig', ["current_page" => "itemCreate", "post" => $_POST, "liste" => $liste]);
+        } else {
+            $this->render($response, 'pages/404.twig', ["current_page" => "404"]);
         }
     }
 
@@ -63,36 +54,6 @@ class PagesController extends MainController
         }
     }
 
-    public function getListeCreate(RequestInterface $request, ResponseInterface $response)
-    {
-        $this->render($response, 'pages/listeCreate.twig', ["current_page" => "listeCreate", "post" => $_POST]);
-    }
-
-    public function getItemCreate(RequestInterface $request, ResponseInterface $response)
-    {
-        $get = explode("=", $request->getUri()->getQuery());
-        $liste = Liste::where('token', $get[1]);
-        if ($liste->count() == 1) {
-            $liste = $liste->first();
-            $this->render($response, 'pages/itemCreate.twig', ["current_page" => "itemCreate", "post" => $_POST, "liste" => $liste]);
-        } else {
-            $this->render($response, 'pages/404.twig', ["current_page" => "404"]);
-        }
-    }
-
-    public function getListeEditor(RequestInterface $request, ResponseInterface $response)
-    {
-        $get = explode("=", $request->getUri()->getQuery());
-        $liste = Liste::where('token', $get[1]);
-
-        if ($liste->count() == 1) {
-            $liste = $liste->first();
-            $this->render($response, 'pages/listeEditor.twig', ["current_page" => "listeEditor", "post" => $_POST, "liste" => $liste]);
-        } else {
-            $this->render($response, 'pages/404.twig', ["current_page" => "404"]);
-        }
-    }
-
     public function getItemEditor(RequestInterface $request, ResponseInterface $response)
     {
         $get = explode("=", $request->getUri()->getQuery());
@@ -112,38 +73,13 @@ class PagesController extends MainController
     }
 
     public
-    function postListe(RequestInterface $request, ResponseInterface $response)
-    {
-        $get = explode("=", $request->getUri()->getQuery());
-        if (isset($get[1])) {
-            $liste = Liste::where('token', $get[1])
-                ->update(['titre' => strip_tags($_POST['titre']),
-                    'description' => strip_tags($_POST['description']),
-                    'expiration' => strip_tags($_POST['expiration'])]);
-        } else {
-            $liste = new Liste();
-            $lastId = Liste::all()->count();
-            $liste->user_id = $lastId + 1;
-            $liste->token = "nosecure" . ($lastId + 1);
-            $liste->titre = strip_tags($_POST['titre']);
-            $liste->description = strip_tags($_POST['description']);
-            $liste->expiration = strip_tags($_POST['expiration']);
-            $liste->save();
-        }
-
-        //$this->redirect($response, 'home');
-        $this->render($response, 'pages/home.twig', ["current_page" => "home"]);
-    }
-
-    public
     function getItem(RequestInterface $request, ResponseInterface $response)
     {
         $get = explode("=", $request->getUri()->getQuery());
         $this->render($response, 'pages/items.twig', ['current_page' => "item"]);
     }
 
-    public
-    function postItem(RequestInterface $request, ResponseInterface $response)
+    public function postItem(RequestInterface $request, ResponseInterface $response)
     {
         $get = explode("=", $request->getUri()->getQuery());
         if ($get[0] == "item") {
@@ -190,8 +126,7 @@ class PagesController extends MainController
         $this->render($response, 'pages/home.twig', ["current_page" => "home"]);
     }
 
-    public
-    function deleteItem(RequestInterface $request, ResponseInterface $response)
+    public function deleteItem(RequestInterface $request, ResponseInterface $response)
     {
         $get = explode("=", $request->getUri()->getQuery());
         $item = Item::where('id', $get[1]);
